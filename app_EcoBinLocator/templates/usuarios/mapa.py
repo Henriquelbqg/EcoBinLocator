68f4b1ba99d2180e570da67d07c66eae34811e08
rodlucenac <132632548+rodlucenac@users.noreply.github.com>
2023-10-28 19:51:37 -0300

    update commit

--- app_EcoBinLocator/templates/usuarios/mapa.py
+++ app_EcoBinLocator/templates/usuarios/mapa.py
@@ -1,5 +1,6 @@
 import folium
 import webbrowser
+from folium import plugins
 
 latitude_padrao = -8.0476
 longitude_padrao = -34.8770
@@ -14,6 +15,15 @@ latitude3 = -8.03047
 longitude3 = -34.9230
 latitude4 = -8.03174
 longitude4 = -34.9597
+latitude5 = -8.022919
+longitude5 = -34.9188560
+latitude6 = -8.08012900
+longitude6 = -34.90513900
+latitude7 = -8.0669835
+longitude7 = -34.8818142
+latitude8 = -8.1139861
+longitude8 = -35.0238781
+
 coordenadas_lixo1 = [latitude1, longitude1]
 icon1 = folium.CustomIcon(icon_image='imagem_lixo.png', icon_size=(30, 30))
 lixo1 = folium.Marker(coordenadas_lixo1, icon=icon1, popup='R. Manoel de Brito, 871')
@@ -30,7 +40,21 @@ coordenadas_lixo4 = [latitude4, longitude4]
 icon4 = folium.CustomIcon(icon_image='imagem_lixo.png', icon_size=(30, 30))
 lixo4 = folium.Marker(coordenadas_lixo4, icon=icon4, popup='R. Gov. Leopoldo Neves, 271')
 lixo4.add_to(mapa)
-
-mapa.save('mapa_sem_endereco.html')
-
-webbrowser.open('mapa_sem_endereco.html')
+coordenadas_lixo5 = [latitude5, longitude5]
+icon5 = folium.CustomIcon(icon_image='recycling.png', icon_size=(25, 25))
+lixo5 = folium.Marker(coordenadas_lixo5, icon=icon5, popup='Rua Padre Lemos, 350')
+lixo5.add_to(mapa)
+coordenadas_lixo6 = [latitude6, longitude6]
+icon6 = folium.CustomIcon(icon_image='recycling.png', icon_size=(25, 25))
+lixo6 = folium.Marker(coordenadas_lixo6, icon=icon6, popup='Rua da Paz, 283')
+lixo6.add_to(mapa)
+coordenadas_lixo7 = [latitude7, longitude7]
+icon7 = folium.CustomIcon(icon_image='recycling.png', icon_size=(25, 25))
+lixo7 = folium.Marker(coordenadas_lixo7, icon=icon7, popup='Rua da Concórdia, 176')
+lixo7.add_to(mapa)
+coordenadas_lixo8 = [latitude8, longitude8]
+icon8 = folium.CustomIcon(icon_image='recycling.png', icon_size=(25, 25))
+lixo8 = folium.Marker(coordenadas_lixo8, icon=icon8, popup='Avenida Barão de Lucena, 593 ')
+lixo8.add_to(mapa)
+mapa.save('mapa.html')
+webbrowser.open('mapa.html')
