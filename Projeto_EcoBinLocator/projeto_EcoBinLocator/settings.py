d1de3a8df6b2f90a5b52309a3650c00247c0ff80
rodlucenac <digolucena14@gmail.com>
2023-11-26 21:07:45 -0300

    upload static

--- Projeto_EcoBinLocator/projeto_EcoBinLocator/settings.py
+++ Projeto_EcoBinLocator/projeto_EcoBinLocator/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # STATIC_URL = "static/"
-STATIC_URL = os.environ.get('DJANGO_STATIC_URL', "/static/")
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
