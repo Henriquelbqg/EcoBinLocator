17f64a3492fe1741c4c04bd74613f8c6ca4b8ee9
rodlucenac <digolucena14@gmail.com>
2023-11-27 20:19:09 -0300

    Update tests.py

--- Projeto_EcoBinLocator/app_EcoBinLocator/tests.py
+++ Projeto_EcoBinLocator/app_EcoBinLocator/tests.py
@@ -15,6 +15,7 @@ from selenium.webdriver.support import expected_conditions as EC
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--headless")
 browser = webdriver.Chrome(options=chrome_options)
 
 
