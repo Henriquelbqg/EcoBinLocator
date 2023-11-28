f9ba81c7a111c20e1023fb82ec8645ceee368e52
rodlucenac <digolucena14@gmail.com>
2023-11-27 21:04:45 -0300

    update tests

--- Projeto_EcoBinLocator/app_EcoBinLocator/tests.py
+++ Projeto_EcoBinLocator/app_EcoBinLocator/tests.py
@@ -12,10 +12,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-
 chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-dev-shm-usage")
 browser = webdriver.Chrome(options=chrome_options)
 
 
